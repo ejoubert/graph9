dcded579b6b68f84a53e8548cf9aa14db779bb7b
JamesSM <jsummerb@gmail.com>
2018-07-23 14:31:39 -0300

    Changing structure

--- app/routes/visualization.js
+++ app/routes/visualization.js
@@ -6,33 +6,48 @@ export default Route.extend({
   model() {
     let query = 'match (n:Person)-[r]-(m:Ideal_Opera) return distinct n,m,r limit 10'
     return this.get('neo4j.session')
-          .run(query)
-          .then(function (result) {
-            let nodes = []
-            for (let i = 0; i < result.records.length; i++){
-              let performance = {
-                title: result.records[i].toObject().m.properties.Ideal_Opera,
-                composer: result.records[i].toObject().n.properties.Composer,
-                composer_id: result.records[i].toObject().n.identity.low,
-                opera_id: result.records[i].toObject().m.identity.low,
-              }
-              nodes.push(performance)
-            }
-            let composers= [];
-            for (var i = 0; i < nodes.length; i++) { 
-              composers.push({
-                composer: nodes[i].composer,
-                composer_id: nodes[i].composer_id
-              })
-            }
-            console.log({
-              composers: composers.uniqBy('composer_id'),
-              operas: nodes.uniqBy('opera_id')
-            })
-            return {
-              composers: composers.uniqBy('composer_id'),
-              operas: nodes.uniqBy('opera_id')
-            }
-          })
+    .run(query)
+    .then(function (result) {
+      let nodes = []
+      let performance = []
+
+ // If Person
+      if (result.records[0].toObject()['n'].labels == 'Person') {
+        console.log(result.records[0].toObject()['n'].labels.toString())
+        for (let i = 0; i < result.records.length; i++){
+          performance = {
+            name: result.records[i].toObject().m.properties.Ideal_Opera,
+            properties: result.records[i].toObject().n.properties,
+          }
+          nodes.push(performance)
+          console.log(nodes)
+        }
+      }
+
+
+
+
+
+      let composers= [];
+      for (var i = 0; i < nodes.length; i++) { 
+        composers.push({
+          composer: nodes[i].composer,
+          composer_id: nodes[i].composer_id
+        })
+      }
+      console.log({
+        composers: composers.uniqBy('composer_id'),
+        operas: nodes.uniqBy('opera_id')
+      })
+      return {
+        composers: composers.uniqBy('composer_id'),
+        operas: nodes.uniqBy('opera_id')
+      }
+    })
   }
 });
+
+
+// Make a generic code that gathers all the information needed to display the node in vis.js and also be editable in it's node-view component.
+
+    // Re-structure the final object so that each node has a node.name as well as a node.properties
