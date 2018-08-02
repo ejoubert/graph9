4c4caa3a2a1907089e8f39403afc92b2878e7669
JamesSM <jsummerb@gmail.com>
2018-08-02 11:42:50 -0300

    Removed old actions

--- app/components/canvas-toolbar.js
+++ app/components/canvas-toolbar.js
@@ -2,22 +2,4 @@ import Component from '@ember/component';
 import {inject as service} from '@ember/service'
 
 export default Component.extend({
-  graphCache: service('graph-data-cache'),
-
-  actions: {
-    addTestNode() {
-      const graphCache = this.get('graphCache');
-      let newNode = {
-        name: "Test Node",
-        isNode: true,
-        id: 999
-      }
-      graphCache.add(newNode)
-    },
-    addDataSet() {
-      const graphCache = this.get('graphCache');
-      let query = 'match(n) return n limit 100'
-      graphCache.query(query)
-    }
-  }
 });
