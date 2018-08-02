e831f420525ce6515fa20773a358d46d3b7870af
JamesSM <jsummerb@gmail.com>
2018-08-02 11:43:05 -0300

    Removed old actions

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -7,26 +7,8 @@ import {inject as service} from '@ember/service';
 
 export default Controller.extend({
   projectName: 'Visualizing Operatic Fame',
-  rb: service('relationship-builder'),
+  // rb: service('relationship-builder'),
   neo4j: service('neo4j-connection'),
   types: ['Performance_Of', 'Performed_By', 'Performed_In', 'References', 'Wrote'],
   choice: 'Choose a Relationship Type...',
-
-  actions: {
-    submit(source, destination) {
-      console.log('relationship type: ' + this.get('choice'))
-      console.log('source: ' + source)
-      console.log('destination: ' + destination)
-      this.get('rb').set('showModal', false)
-      let query = 'MATCH(n),(m) WHERE id(n) ='+source+' AND id(m) ='+destination+' MERGE (n)-[:'+this.get('choice')+']->(m)'
-      return this.get('neo4j.session')
-      .run(query)
-      .then(function (result) {
-        console.log(result)
-      })
-    },
-    chooseType(type) {
-      this.set('choice', type)
-    }
-  }
 });
