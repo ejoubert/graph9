8fa7077ec1f6606600b19576db150610d79041cc
JamesSM <jsummerb@gmail.com>
2018-10-27 00:40:05 -0300

    better application template and controller

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -5,23 +5,11 @@ export default Controller.extend({
   router: service('router'),
   graphCache: service('graph-data-cache'),
 
-  projectName: null,
-  neo4j: null,
-  types: null,
-  choice: null,
-  login: false,
-
-  init() {
-    this._super(...arguments)
-    this.set('projectName', 'Graph9')
-    this.set('neo4j', service('neo4j-connection'))
-    this.set('choices', 'Choose a Relationship Type...')
-  },
+  projectName: "Graph9",
 
   actions: {
-
     logout() {
-      this.get('router').transitionTo('/login')
+      this.get('router').transitionTo('login')
       this.get('graphCache').empty()
       this.set('login', false)
     }
