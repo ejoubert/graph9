bbbcb6e7ab3054aecda006de22a3e73348b27fbf
JamesSM <jsummerb@gmail.com>
2018-10-28 18:22:36 -0300

    added actions to enable/disable user guide from user-guide

---
+++ app/controllers/visualization.js
@@ -0,0 +1,14 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+
+  actions: {
+    showGuide() {
+      this.set('showGuide', true)
+    },
+
+    closeModal() {
+      this.set('showGuide', false)
+    },
+  }
+});
