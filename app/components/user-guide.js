a2cf49bfcd7dedf15f23cc4a18295a664bc44357
JamesSM <jsummerb@gmail.com>
2018-10-30 23:06:07 -0300

    Added user-guide

--- app/components/user-guide.js
+++ app/components/user-guide.js
@@ -1,4 +1,21 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  labelTypes: null,
+  labelChoice: "Label one",
+
+  init() {
+    this._super(...arguments)
+    this.set('labelTypes', ["Label one", "Label two", "Label three"])
+  },
+
+  actions: {
+    chooseLabel(label) {
+      this.set('labelChoice', label)
+    },
+
+    toggleEditing() {
+      this.toggleProperty('isEditing')
+    }
+  }
 });
