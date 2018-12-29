1e1590abd6e8ec3b5a6e90e21705cfb23aa79bf2
James <jsummerb@gmail.com>
2018-12-28 23:30:17 -0400

    added redirected to model hook

--- app/routes/visualization.js
+++ app/routes/visualization.js
@@ -18,6 +18,15 @@ export default Route.extend({
   },
 
   model () {
+    this.graphCache.login().then((result) => {
+      if (!result) {
+        const graphCache = this.graphCache
+        graphCache.init()
+        this.router.transitionTo('visualization')
+      } else {
+        this.router.transitionTo('welcome')
+      }
+    })
     const graphCache = this.graphCache
     return graphCache.query()
   },
