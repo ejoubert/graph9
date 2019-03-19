e3d4e79e79824f76d0a1f6eba7b04e75e329cfcb
James <jsummerb@gmail.com>
2019-03-19 16:50:33 -0300

    redirects to visualization if already logged in

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,4 +1,21 @@
 import Route from '@ember/routing/route'
+import { inject as service } from '@ember/service'
 
 export default Route.extend({
+  graphCache: service('graph-data-cache'),
+  router: service(),
+
+  beforeModel() {
+    this.graphCache.login().then((result) => {
+      if (!result) {
+        const graphCache = this.graphCache
+        graphCache.init()
+        this.router.transitionTo('visualization')
+      } else {
+        this.router.transitionTo('welcome')
+      }
+    })
+    const graphCache = this.graphCache
+    return graphCache.query()
+  }
 })
--- app/routes/visualization.js
+++ app/routes/visualization.js
@@ -5,17 +5,6 @@ export default Route.extend({
   graphCache: service('graph-data-cache'),
   router: service(),
 
-  beforeModel () {
-    this.graphCache.login().then((result) => {
-      if (!result) {
-        const graphCache = this.graphCache
-        graphCache.init()
-        this.router.transitionTo('visualization')
-      } else {
-        this.router.transitionTo('welcome')
-      }
-    })
-  },
 
   model () {
     this.graphCache.login().then((result) => {
