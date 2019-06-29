1b591f84a349526ba98d10928a974949f73c9d4e
James Summerby-Murray <jsummerb@gmail.com>
2019-06-29 14:30:43 -0300

    remove sentry for dev

--- app/app.js
+++ app/app.js
@@ -2,8 +2,8 @@ import Application from '@ember/application'
 import Resolver from './resolver'
 import loadInitializers from 'ember-load-initializers'
 import config from './config/environment'
-import * as Sentry from '@sentry/browser'
-import * as Integrations from '@sentry/integrations';
+// import * as Sentry from '@sentry/browser'
+// import * as Integrations from '@sentry/integrations';
 
 const App = Application.extend({
   modulePrefix: config.modulePrefix,
@@ -12,10 +12,10 @@ const App = Application.extend({
 
 })
 
-Sentry.init({
-  dsn: 'https://b2b59f389b144d9ebb4f865d0d4563a5@sentry.io/1485605',
-  integrations: [new Integrations.Ember()]
-});
+// Sentry.init({
+//   dsn: 'https://b2b59f389b144d9ebb4f865d0d4563a5@sentry.io/1485605',
+//   integrations: [new Integrations.Ember()]
+// });
 
 loadInitializers(App, config.modulePrefix)
 
