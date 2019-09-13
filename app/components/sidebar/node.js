f7ed324867ef30a31f177960a4a64da14f07b49a
James Summerby-Murray <jsummerb@gmail.com>
2019-09-13 13:46:58 -0300

    consistent sidebar structure

--- app/components/sidebar/node.js
+++ app/components/sidebar/node.js
@@ -1,3 +1,28 @@
 import Component from '@ember/component';
+import { action } from '@ember/object'
 
-export default class SideBarNode extends Component {}
+export default class SideBarNode extends Component {
+  classNames = ['node-base']
+
+  addingNewProperty = false
+
+  changes = {
+    properties: {
+      keys: {
+      },
+      values: {
+      }
+    }
+  }
+
+  didInsertElement() {
+    if (this.node) {
+      this.set('originalNode', JSON.parse(JSON.stringify(this.node)))
+    }
+  }
+
+  @action
+  save() {
+    this.saveNode(this.node, this.changes, this.originalNode)
+  }
+}
